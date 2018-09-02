bdafcf43d1490d2bd1f9b8fa1d68a7f912c40518
George <7jagjag@gmail.com>
2018-09-02 13:17:00 +0200

    improve key binding and use a json viewer

--- src/App.js
+++ src/App.js
@@ -3,32 +3,82 @@ import Tabs from './comps/Tabs';
 import API from './comps/API';
 import Textarea from './comps/Textarea';
 import Home from './comps/Home';
+import JSONView from './jsonview/App';
 const ShowIf = ({ value, children }) => (value ? children : null);
-
-const style = {
-  fontSize: '20px',
-  width: '100%',
-  height: '75vh',
-  fontFamily: 'monospace',
-  border: '1px solid #ddd',
-};
+const VERSION = process.env.REACT_APP_VERSION;
 
 export default class App extends PureComponent {
   state = {
     sidepane: true,
-    data: '',
-    callback: () => ({}),
+    tab: '',
+    tabsSaveable: ['/rules', '/tosca-conf', '/config'],
+    contents: {},
+    saveData: () => console.log('savedata: not binded'),
+    runFN: () => console.log('run: not binded'),
   };
 
   componentDidMount() {
-    document.onkeypress = ({ code, altKey }) => {
-      if (code === 'Backquote') {
+    let previousKey = '';
+    let pressedKeys = [];
+    const getLastN = (a, n) => a.slice(a.length - n);
+    document.onkeydown = e => {
+      const { code } = e;
+
+      // add to our history of pressed keys
+      pressedKeys.push(code);
+
+      // truncate array if length is over 5
+      if (pressedKeys.length > 5) pressedKeys = getLastN(pressedKeys, 5);
+
+      // create a pattern: join last 3 pressed keys
+      let pattern = '';
+      if (pressedKeys.length >= 3) {
+        const keys = getLastN(pressedKeys, 3);
+        pattern = keys.join('-');
+      }
+
+      // check pattern: CMD+K+B
+      if (pattern === 'MetaLeft-KeyK-KeyB') {
         this.togglePane();
       }
+
+      if (previousKey === 'MetaLeft') {
+        if (code === 'KeyS') {
+          e.preventDefault();
+          const data = this.state.contents[`/${this.state.tab}`];
+          if (data) {
+            //save
+            console.log(`saving ${this.state.tab} ...`);
+            this.state.saveData();
+          }
+        }
+        if (code === 'KeyR') {
+          e.preventDefault();
+          console.log(`running ...`);
+          this.state.runFN();
+        }
+      }
+
+      previousKey = code;
     };
   }
 
   togglePane = () => this.setState(s => ({ ...s, sidepane: !s.sidepane }));
+  onSetTab = ({ tab }) => this.setState({ tab });
+  bindSave = saveData => this.setState({ saveData });
+  bindRun = runFN => this.setState({ runFN });
+  bindStateData = ({ endpoint, data }) => {
+    if (this.state.tabsSaveable.indexOf(endpoint) === -1) {
+      return;
+    }
+    this.setState(s => ({
+      ...s,
+      contents: {
+        ...s.contents,
+        [endpoint]: data,
+      },
+    }));
+  };
 
   render() {
     const { sidepane } = this.state;
@@ -40,19 +90,27 @@ export default class App extends PureComponent {
               className={sidepane ? 'col-sm-3' : 'col-sm-12'}
               style={{ height: '90vh' }}
             >
-              <Home sidepane={sidepane} togglePane={this.togglePane} />
+              <Home
+                bindRun={this.bindRun}
+                sidepane={sidepane}
+                togglePane={this.togglePane}
+              />
             </div>
             <ShowIf value={sidepane}>
               <div
                 className="col-sm-9"
                 style={{ borderLeft: 'solid #aaa 1px', height: '90vh' }}
               >
-                <Tabs>
+                <Tabs onSetTab={this.onSetTab}>
                   <Tabs.Tab title="home">
-                    <h2>Welcome v1.0.0</h2>
+                    <h2>Welcome v{VERSION}</h2>
                   </Tabs.Tab>
                   <Tabs.Tab title="tosca-conf">
-                    <API endpoint="/tosca-conf">
+                    <API
+                      endpoint="/tosca-conf"
+                      bindSave={this.bindSave}
+                      onData={this.bindStateData}
+                    >
                       {({ data, saveData, onUpdate }) => (
                         <Fragment>
                           <button
@@ -68,16 +126,16 @@ export default class App extends PureComponent {
                     </API>
                   </Tabs.Tab>
                   <Tabs.Tab title="rules">
-                    <API endpoint="/rules">
+                    <API
+                      endpoint="/rules"
+                      bindSave={this.bindSave}
+                      onData={this.bindStateData}
+                    >
                       {({ data, saveData, onUpdate }) => (
                         <Fragment>
                           <button
                             className="btn btn-light btn-block"
-                            onClick={() => {
-                              document.onkeypress = ({ code }) =>
-                                code === 'Backquote' ? this.togglePane() : null;
-                              saveData();
-                            }}
+                            onClick={saveData}
                           >
                             save
                           </button>
@@ -88,16 +146,16 @@ export default class App extends PureComponent {
                     </API>
                   </Tabs.Tab>
                   <Tabs.Tab title="config">
-                    <API endpoint="/config">
+                    <API
+                      endpoint="/config"
+                      bindSave={this.bindSave}
+                      onData={this.bindStateData}
+                    >
                       {({ data, saveData, onUpdate }) => (
                         <Fragment>
                           <button
                             className="btn btn-light btn-block"
-                            onClick={() => {
-                              document.onkeypress = ({ code }) =>
-                                code === 'Backquote' ? this.togglePane() : null;
-                              saveData();
-                            }}
+                            onClick={saveData}
                           >
                             save
                           </button>
@@ -108,17 +166,20 @@ export default class App extends PureComponent {
                     </API>
                   </Tabs.Tab>
                   <Tabs.Tab title="output.smt">
-                    <API endpoint="/output.smt">
+                    <API endpoint="/output.smt" onData={this.bindStateData}>
                       {({ data, onUpdate }) => (
                         <Textarea data={data} onUpdate={onUpdate} />
                       )}
                     </API>
                   </Tabs.Tab>
                   <Tabs.Tab title="maindata.json">
-                    <API endpoint="/maindata.json" json>
-                      {({ data, onUpdate }) => (
-                        <pre style={style}>{JSON.stringify(data, null, 2)}</pre>
-                      )}
+                    <API endpoint="/maindata.json" onData={this.bindStateData}>
+                      {({ data }) => <JSONView code={data} />}
+                    </API>
+                  </Tabs.Tab>
+                  <Tabs.Tab title="output.json">
+                    <API endpoint="/output.json" onData={this.bindStateData}>
+                      {({ data }) => <JSONView code={data} />}
                     </API>
                   </Tabs.Tab>
                 </Tabs>
